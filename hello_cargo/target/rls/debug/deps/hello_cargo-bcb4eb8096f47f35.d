86e88f6ff7c653a27fb9518cfa18b2d78b1beb98
Eric Dahlgren <eric@nodeiwest.se>
2021-10-09 11:26:50 +0200

    create hello_cargo

---
+++ hello_cargo/target/rls/debug/deps/hello_cargo-bcb4eb8096f47f35.d
@@ -0,0 +1,5 @@
+c:\Users\eric.dahlgren\Documents\GitHub\learn-rust\hello_cargo\target\rls\debug\deps\hello_cargo-bcb4eb8096f47f35.rmeta: src\main.rs
+
+c:\Users\eric.dahlgren\Documents\GitHub\learn-rust\hello_cargo\target\rls\debug\deps\hello_cargo-bcb4eb8096f47f35.d: src\main.rs
+
+src\main.rs:
