d199c90a5db87392dcd72005d971c4d6155131aa
eric <50216491+Eriyc@users.noreply.github.com>
2021-10-09 11:33:11 +0200

    Delete hello_cargo/target directory

--- hello_cargo/target/rls/debug/deps/hello_cargo-40ceb403431c7a3e.d
+++
@@ -1,5 +0,0 @@
-c:\Users\eric.dahlgren\Documents\GitHub\learn-rust\hello_cargo\target\rls\debug\deps\hello_cargo-40ceb403431c7a3e.rmeta: src\main.rs
-
-c:\Users\eric.dahlgren\Documents\GitHub\learn-rust\hello_cargo\target\rls\debug\deps\hello_cargo-40ceb403431c7a3e.d: src\main.rs
-
-src\main.rs:
